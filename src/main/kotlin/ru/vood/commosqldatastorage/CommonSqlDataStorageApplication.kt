f5e352262e1b361ec326943899f2ab49a05ff607
Vood <balitsky_igor@list.ru>
2020-11-02 19:48:55 +0300

    init

--- src/main/kotlin/ru/vood/commosqldatastorage/CommonSqlDataStorageApplication.kt
+++ src/main/kotlin/ru/vood/commosqldatastorage/CommonSqlDataStorageApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CommoSqlDataStorageApplication
+class CommonSqlDataStorageApplication
 
 fun main(args: Array<String>) {
-    runApplication<CommoSqlDataStorageApplication>(*args)
+    runApplication<CommonSqlDataStorageApplication>(*args)
 }
